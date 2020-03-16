859f28a16b4a48fa1cd38cd0bbf885a5cb8633b3
sudo-security <61291453+sudo-security@users.noreply.github.com>
2020-03-16 19:17:39 +0300

    Add files via upload

---
+++ rmpack.sh
@@ -0,0 +1,29 @@
+echo '
+
+[+]-----------------------------------------------------------------[+]
+         _             _           ___  _       _   _____      
+        | |_   _ _ __ | | ___ __  / _ \(_)_ __ | |_|___ / _ __ 
+     _  | | | | |  _ \| |/ /  _ \| | | | |  _ \| __| |_ \|  __|
+    | |_| | |_| | | | |   <| |_) | |_| | | | | | |_ ___) | |   
+     \___/ \__ _|_| |_|_|\_\ .__/ \___/|_|_| |_|\__|____/|_|   
+                           |_|     
+ 
+[+]-----------------[Reverse Toolpack Removation]--------------------[+]
+
+               [~]------[Thanks For Using]------[~]
+
+
+[-] Twitter.com/BShellcode
+[-] www.sudo-security.github.io
+
+'
+rm -rf aslr
+rm -rf checkend
+rm -rf pattern_fuzzer
+rm -rf pattern_offset
+rm -rf peda
+rm -rf suid_dump
+rm -rf rpack-uninstall
+clear
+
+echo '[!] Basariyla Kaldirildi..!'
---
+++ rpack-uninstall.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+system("bash rmpack.sh");
+return 0;	
+}
